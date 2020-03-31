547b90df73e8215451c13534158dc6db18b6dfd4
Yuankai Huo <yuankaihuo@users.noreply.github.com>
2020-03-30 23:04:59 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# example_UNet
+# UNet: semantic segmentation with PyTorch
+
+> Forked From github.com/milesial
+
+## Glomerulus Segmentation Notes
+
+#### Changes to Repository and Summary
+
+_03/20_:
+
+* Obtained Dice Score > 0.835 via Standard Segmentation of UNET provided by repository .
+
+_3/27_:
+
+* Fixed issue of random-selected Validation images `train.py`; using roughly 20% of the established training images as the Validation images.
+  * Validation dataset is roughly 196 images.
+* Visualized data via `tensorboard`, image shown here:
+* Selected best model `.pth` file to analyze testing with.
+* Tested data and composed three images together.
+
+## Usage
+**Note : Use Python 3**
+### Prediction
+
+You can easily test the output masks on your images via the CLI.
+
+To predict a single image and save it:
+
+`python predict.py -i image.jpg -o output.jpg`
+
+To predict a multiple images and show them without saving them:
+
+`python predict.py -i image1.jpg image2.jpg --viz --no-save`
+
+```shell script
+> python predict.py -h
+usage: predict.py [-h] [--model FILE] --input INPUT [INPUT ...]
+                  [--output INPUT [INPUT ...]] [--viz] [--no-save]
+                  [--mask-threshold MASK_THRESHOLD] [--scale SCALE]
+
+Predict masks from input images
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --model FILE, -m FILE
+                        Specify the file in which the model is stored
+                        (default: MODEL.pth)
+  --input INPUT [INPUT ...], -i INPUT [INPUT ...]
+                        filenames of input images (default: None)
+  --output INPUT [INPUT ...], -o INPUT [INPUT ...]
+                        Filenames of ouput images (default: None)
+  --viz, -v             Visualize the images as they are processed (default:
+                        False)
+  --no-save, -n         Do not save the output masks (default: False)
+  --mask-threshold MASK_THRESHOLD, -t MASK_THRESHOLD
+                        Minimum probability value to consider a mask pixel
+                        white (default: 0.5)
+  --scale SCALE, -s SCALE
+                        Scale factor for the input images (default: 0.5)
+```
+You can specify which model file to use with `--model MODEL.pth`.
+
+### Training
+
+```shell script
+> python train.py -h
+usage: train.py [-h] [-e E] [-b [B]] [-l [LR]] [-f LOAD] [-s SCALE] [-v VAL]
+
+Train the UNet on images and target masks
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -e E, --epochs E      Number of epochs (default: 5)
+  -b [B], --batch-size [B]
+                        Batch size (default: 1)
+  -l [LR], --learning-rate [LR]
+                        Learning rate (default: 0.1)
+  -f LOAD, --load LOAD  Load model from a .pth file (default: False)
+  -s SCALE, --scale SCALE
+                        Downscaling factor of the images (default: 0.5)
+  -v VAL, --validation VAL
+                        Percent of the data that is used as validation (0-100)
+                        (default: 15.0)
+
+```
+By default, the `scale` is 0.5, so if you wish to obtain better results (but use more memory), set it to 1.
+
+The input images and target masks should be in the `data/imgs` and `data/masks` folders respectively.
+
+## Tensorboard
+You can visualize in real time the train and test losses, the weights and gradients, along with the model predictions with tensorboard:
+
+`tensorboard --logdir=runs`
---
+++ train.py
@@ -0,0 +1,271 @@
+import argparse
+import logging
+import os
+import sys
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+from tqdm import tqdm
+
+from eval import eval_net
+from unet import UNet
+
+import torch.nn.functional as F
+def cross_entropy2d(input, target, weight=None, size_average=True):
+    # input: (n, c, h, w), target: (n, h, w)
+    n, c, h, w = input.size()
+    # log_p: (n, c, h, w)
+    log_p = F.log_softmax(input, dim=1)
+    # log_p: (n*h*w, c)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    ind_p = target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0
+    log_p = log_p[ind_p.view(-1,c )]
+    log_p = log_p.view(-1, c)
+    # target: (n*h*w,)
+    mask = target >= 0
+    target = target[mask]
+    loss = F.nll_loss(log_p, target, weight=weight, size_average=False)
+    if size_average:
+        loss /= mask.data.sum()
+    return loss
+
+def dice_loss(input, target):
+    """
+    input is a torch variable of size BatchxnclassesxHxW representing log probabilities for each class
+    target is a 1-hot representation of the groundtruth, shoud have same size as the input
+    """
+    assert input.size() == target.size(), "Input sizes must be equal."
+    assert input.dim() == 4, "Input must be a 4D Tensor."
+    # uniques = np.unique(target.numpy())
+    # assert set(list(uniques)) <= set([0, 1]), "target must only contain zeros and ones"
+
+    probs = F.softmax(input, dim=1)
+    num = probs * target  # b,c,h,w--p*g
+    num = torch.sum(num, dim=3)
+    num = torch.sum(num, dim=2)  # b,c
+
+    den1 = probs * probs  # --p^2
+    den1 = torch.sum(den1, dim=3)
+    den1 = torch.sum(den1, dim=2)  # b,c,1,1
+
+    den2 = target * target  # --g^2
+    den2 = torch.sum(den2, dim=3)
+    den2 = torch.sum(den2, dim=2)  # b,c,1,1
+
+    dice = 2 * ((num+0.0000001) / (den1 + den2+0.0000001))
+    dice_eso = dice[:, 1]  # we ignore bg dice val, and take the fg
+
+    dice_total = -1 * torch.sum(dice_eso) / dice_eso.size(0)  # divide by batch_sz
+
+    return dice_total
+
+
+
+#yuankai change to tensorboard
+from tensorboardX import SummaryWriter
+from utils.dataset import BasicDataset
+from torch.utils.data import DataLoader, random_split
+
+dir_img = 'data/imgs/'
+dir_mask = 'data/masks/'
+dir_valimg = 'data/imgval/'
+dir_valmask = 'data/masksval/'
+dir_checkpoint = 'checkpoints/'
+
+def train_net(net,
+              device,
+              epochs=250,
+              batch_size=4,
+              lr=0.0001,
+              val_percent=0.2,
+              save_cp=True,
+              img_scale=1):
+
+    dataset = BasicDataset(dir_img, dir_mask, img_scale)
+    dataval = BasicDataset(dir_valimg, dir_valmask, img_scale)
+
+    # yuankai change it to automated
+    # direct sizes of each training. 
+    n_val = dataval.__len__()
+    n_train = dataset.__len__()
+
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+    val_loader = DataLoader(dataval, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=True, drop_last=True)
+
+    writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
+    global_step = 0
+
+    logging.info(f'''Starting training:
+        Epochs:          {epochs}
+        Batch size:      {batch_size}
+        Learning rate:   {lr}
+        Training size:   {n_train}
+        Validation size: {n_val}
+        Checkpoints:     {save_cp}
+        Device:          {device.type}
+        Images scaling:  {img_scale}
+    ''')
+
+    #yuankai change the optimizer to Adam
+    # optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=lr, betas=(0.9, 0.999))
+    #yuankai remove scheduler
+    # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min' if net.n_classes > 1 else 'max', patience=2)
+    if net.n_classes > 1:
+        criterion = nn.CrossEntropyLoss()
+    else:
+        criterion = nn.BCEWithLogitsLoss()
+
+    for epoch in range(epochs):
+        net.train()
+
+        epoch_loss = 0
+        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                imgs = batch['image']
+                true_masks = batch['mask']
+                #yuankai add true_masks_2channel for calculating dice loss
+                true_masks_2channel = true_masks.unsqueeze(1)
+                true_masks_2channel = torch.cat((1-true_masks_2channel, true_masks_2channel), 1)
+                true_masks_2channel = true_masks_2channel.to(device=device, dtype=torch.float32)
+
+                assert imgs.shape[1] == net.n_channels, \
+                    f'Network has been defined with {net.n_channels} input channels, ' \
+                    f'but loaded images have {imgs.shape[1]} channels. Please check that ' \
+                    'the images are loaded correctly.'
+
+                imgs = imgs.to(device=device, dtype=torch.float32)
+
+                if net.n_classes == 1:
+                    mask_type = torch.float32
+                else:
+                    mask_type = torch.long
+
+                true_masks = true_masks.to(device=device, dtype=mask_type)
+                masks_pred = net(imgs)
+                loss_cross = criterion(masks_pred, true_masks)
+                #yuankai add the dice loss
+                loss_dice = 1 + dice_loss(masks_pred, true_masks_2channel)
+                #yuankai sum the two loss together as the final loss
+                loss = loss_dice + loss_cross
+
+                epoch_loss += loss.item()
+                writer.add_scalar('Loss/train', loss.item(), global_step)
+
+                pbar.set_postfix(**{'loss (batch)': loss.item(), 'loss_cr (batch)': loss_cross.item(), 'loss_dsc (batch)': loss_dice.item()})
+                # pbar.set_postfix(**{'loss2 (batch)': loss2.item()})
+
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_value_(net.parameters(), 0.1)
+                optimizer.step()
+
+
+
+                pbar.update(imgs.shape[0])
+
+                global_step += 1
+                if global_step % (len(dataset) // (10 * batch_size)) == 0:
+                    for tag, value in net.named_parameters():
+                        tag = tag.replace('.', '/')
+                        writer.add_histogram('weights/' + tag, value.data.cpu().numpy(), global_step)
+                        writer.add_histogram('grads/' + tag, value.grad.data.cpu().numpy(), global_step)
+                    val_score = eval_net(net, val_loader, device)
+                    # scheduler.step(val_score)
+
+                    writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], global_step)
+
+                    if net.n_classes > 1:
+                        logging.info('Validation Dice Coeff: {}'.format(val_score))
+                        writer.add_scalar('Loss/test', val_score, global_step)
+                    else:
+                        logging.info('Validation Dice Coeff: {}'.format(val_score))
+                        writer.add_scalar('Dice/test', val_score, global_step)
+
+                    writer.add_images('images', imgs, global_step)
+                    if net.n_classes == 1:
+                        writer.add_images('masks/true', true_masks, global_step)
+                        writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
+                    else:
+                        writer.add_images('masks/true', true_masks.unsqueeze(1), global_step)
+                        writer.add_images('masks/pred', masks_pred.max(dim=1)[1].unsqueeze(1), global_step)
+                        # writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
+
+        if save_cp and (epoch + 1) % 10 == 0:
+            try:
+                os.mkdir(dir_checkpoint)
+                logging.info('Created checkpoint directory')
+            except OSError:
+                pass
+            torch.save(net.state_dict(),
+                       dir_checkpoint + f'CP_epoch{epoch + 1}.pth')
+            logging.info(f'Checkpoint {epoch + 1} saved !')
+
+    writer.close()
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=250,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=4,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=False,
+                        help='Load model from a .pth file')
+    parser.add_argument('-s', '--scale', dest='scale', type=float, default=1,
+                        help='Downscaling factor of the images')
+    parser.add_argument('-v', '--validation', dest='val', type=float, default=10.0,
+                        help='Percent of the data that is used as validation (0-100)')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    args = get_args()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+
+    # Change here to adapt to your data
+    # n_channels=3 for RGB images
+    # n_classes is the number of probabilities you want to get per pixel
+    #   - For 1 class and background, use n_classes=1
+    #   - For 2 classes, use n_classes=2
+    #   - For N > 2 classes, use n_classes=N
+    #yuankai change the number of output channel to 2
+    net = UNet(n_channels=3, n_classes=2)
+    logging.info(f'Network:\n'
+                 f'\t{net.n_channels} input channels\n'
+                 f'\t{net.n_classes} output channels (classes)\n'
+                 f'\t{"Bilinear" if net.bilinear else "Dilated conv"} upscaling')
+
+    if args.load:
+        net.load_state_dict(
+            torch.load(args.load, map_location=device)
+        )
+        logging.info(f'Model loaded from {args.load}')
+
+    net.to(device=device)
+    # faster convolutions, but more memory
+    # cudnn.benchmark = True
+
+    try:
+        train_net(net=net,
+                  epochs=args.epochs,
+                  batch_size=args.batchsize,
+                  lr=args.lr,
+                  device=device,
+                  img_scale=args.scale,
+                  val_percent=args.val / 100)
+    except KeyboardInterrupt:
+        torch.save(net.state_dict(), 'INTERRUPTED.pth')
+        logging.info('Saved interrupt')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
